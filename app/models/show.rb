44657ef5857ee5b56e8286d13bcac36eb9ca623b
Anri <anri0806@hotmail.co.jp>
2022-03-25 16:17:58 -0400

    commit tree e551394843b596f6a40b21997cce25e1fa0149b5

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        #return array of full names of actors associated with show
+        self.actors.map do |r|
+            "#{r.first_name} #{r.last_name}"
+        end
+    end
   
 end
